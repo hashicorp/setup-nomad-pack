8f71740251eb5c3e20b12ea4bcb1c260f4f704bb
Kerim Satirli <k.satirli@gmail.com>
2023-03-09 13:22:11 +0100

    rebuilds to use common GHA format

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import * as core from "@actions/core";
-import {setupBinary} from "./setup-binary";
+import * as hc from "@hashicorp/github-actions-core";
 
 async function main() {
+  const version = core.getInput("version");
+  if (version == null) {
+    core.setFailed("Required field 'version' not set");
+    return;
+  }
   try {
-    await setupBinary();
+    await hc.getHashicorpRelease("nomad-pack", version);
   } catch (error) {
     if (error instanceof Error) {
       core.setFailed(error.message);
